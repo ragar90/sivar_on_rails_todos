199c7514ac7a57410b872f5924ddf4f7401d9622
Rene Garcia <rene.garciah90@gmail.com>
2017-06-02 23:07:53 -0600

    Adding tests for TODOS controller

--- spec/controllers/todos_controller_spec.rb
+++ spec/controllers/todos_controller_spec.rb
@@ -3,18 +3,27 @@ require 'rails_helper'
 RSpec.describe TodosController, type: :controller do
 	describe "GET /todos" do
 		describe "WHEN no parametters are specified" do
-			it "SHOULD retrive a list of unfiltered todos" do
-			end
+			it "SHOULD retrive a list of unfiltered todos" 
 		end
 		describe "WHEN parametter state is specified" do
 			describe "AND is set to 'ACTIVE'" do
-				it "SHOULD retrive a list of 'ACTIVE' todos" do
-				end
+				it "SHOULD retrive a list of 'ACTIVE' todos" 
 			end
 			describe "AND is set to 'INACTIVE'" do
-				it "SHOULD retrive a list of 'INACTIVE' todos" do
-				end
+				it "SHOULD retrive a list of 'INACTIVE' todos" 
 			end
 		end
+	end
+	describe "POST /todos" do
+		it "SHOULD create a new TODO entry"
+	end
+	describe "GET  /todos/:id" do
+		it "SHOULD retrive the TODO corresponding to :id"
+	end
+	describe "PUT  /todos/:id" do
+		it "SHOULD update the TODO corresponding to :id with the supply params"
+	end
+	describe "DELETE /todos/:id" do
+		it "SHOULD delete the TODO corresponding to :id"
 	end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,4 +13,5 @@ RSpec.configure do |config|
   config.shared_context_metadata_behavior = :apply_to_host_groups
   config.include Requests::JsonHelpers, type: :request
   config.include FactoryGirl::Syntax::Methods
+  config.formatter = :documentation
 end
